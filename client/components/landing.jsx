41a99050e817e3b43a8713dd36200113493c4f70
Mason Keiser <masonksr5@gmail.com>
2021-05-08 00:50:39 +0000

    landing styled

--- client/components/landing.jsx
+++ client/components/landing.jsx
@@ -2,10 +2,37 @@ import React from 'react';
 
 const Landing = (props) => {
 
+    const handleMenu = () => {
+        const ham = document.getElementById('side');
+        if (ham.classList.contains('open')) {
+            ham.classList.remove('open') 
+        } else {
+            ham.classList.add('open')
+        }
+    }
+
     return (
         <div className='landingCont'>
+            <div className='copyCont'>
+                <div></div>
+                <h5> Mason Keiser  2021 ©</h5>
+            </div>
+            <div className='shopCont'>
+                <div></div>
+                <h5> Go To Surf Shop</h5>
+            </div>
+            <div className='menuBtn' onClick={() => handleMenu()}>
+                Menu
+            </div>
+            <div className='side shadow-lg' id='side'>
+                <h4>Login</h4>
+                <h4>Sign Up</h4>
+                <h4>View Beaches</h4>
+                <div className='fas fa-chevron-up mt-2' id='menuUp' onClick={() => handleMenu()}></div>
+            </div>
             <div className='logoCont'>
                 <img src="/images/logo4bw.png" alt=""/>
+                <h3 className='viewBtn'>View Beaches</h3>
             </div>
         </div>
     )
