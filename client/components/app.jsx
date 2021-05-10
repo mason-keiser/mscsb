110400fb4751178f43f671083a839562a0a3d890
Mason Keiser <masonksr5@gmail.com>
2021-05-10 17:39:19 +0000

    formatted 'app'

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -8,16 +8,14 @@ import MapPage from './map';
 const App = () => {
 
     return (
-        <div>
-            <Router>
-                <Switch>
-                    <Route exact path='/' render={(props) => (<Landing {...props}/>)}/>
-                    <Route exact path='/signup' render={(props) => (<SignUp {...props}/>)}/>
-                    <Route exact path='/login' render={(props) => (<Login {...props}/>)}/>
-                    <Route exact path='/map' render={(props) => (<MapPage {...props}/>)}/>
-                </Switch>
-            </Router>
-        </div>
+        <Router>
+            <Switch>
+                <Route exact path='/' render={(props) => (<Landing {...props}/>)}/>
+                <Route exact path='/signup' render={(props) => (<SignUp {...props}/>)}/>
+                <Route exact path='/login' render={(props) => (<Login {...props}/>)}/>
+                <Route exact path='/map' render={(props) => (<MapPage {...props}/>)}/>
+            </Switch>
+        </Router>  
     )
 }
 
