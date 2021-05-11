2461dc125c4a7a38970e7ae6d43393078f5f9d2f
Mason Keiser <masonksr5@gmail.com>
2021-05-11 03:54:11 +0000

    responsiveness added to form

--- client/components/signup.jsx
+++ client/components/signup.jsx
@@ -8,29 +8,34 @@ const SignUp = (props) => {
         <div className='signUpCont c'>
             <Menu path={props.match.path} nightMode={props.nightMode} setNightMode={props.setNightMode}/>
             <h2 className='sTitle'>Create Account</h2>
-            <form action="">
-                <div className='wrapper mt-3'>
-                    <div className='input-data' id='e'>
-                        <input required type='text'/>
-                        <label htmlFor="">First Name</label>
+            <div className='swit'>
+                <form action="">
+                    <div className='wrapper mt-3'>
+                        <div className='input-data' id='e'>
+                            <input required type='text'/>
+                            <label htmlFor="">First Name</label>
+                        </div>
+                        <div className='input-data' id='e'>
+                            <input required type='text'/>
+                            <label htmlFor="">Last Name</label>
+                        </div>
+                        <div className='input-data' id='c'>
+                            <input required type='text'/>
+                            <label htmlFor="">Password</label>
+                        </div>
+                        <div className='input-data' id='a'>
+                            <input required type="text"/>
+                            <label htmlFor="">Re Enter Password</label>
+                        </div>
+                        <div>
+                            <button className='signupBtn'>Create Account</button>
+                        </div>
                     </div>
-                    <div className='input-data' id='e'>
-                        <input required type='text'/>
-                        <label htmlFor="">Last Name</label>
-                    </div>
-                    <div className='input-data' id='c'>
-                        <input required type='text'/>
-                        <label htmlFor="">Password</label>
-                    </div>
-                    <div className='input-data' id='a'>
-                        <input required type="text"/>
-                        <label htmlFor="">Re Enter Password</label>
-                    </div>
-                    <div>
-                        <button className='signupBtn'>Create Account</button>
-                    </div>
-                </div>
-            </form>
+                </form>
+            <div className='valCont'>
+
+            </div>
+            </div>
         </div>
     )
 }
