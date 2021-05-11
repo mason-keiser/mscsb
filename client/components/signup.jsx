edf1382a89a8acbbaaedbfa4fff2c727019a8843
Mason Keiser <masonksr5@gmail.com>
2021-05-11 04:47:06 +0000

    started check logic for validation

--- client/components/signup.jsx
+++ client/components/signup.jsx
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom'
 import Menu from './menu'
 
 const SignUp = (props) => {
+    const [firstname, setFirstname] = useState();
+    const [lastname, setLastname] = useState();
+    const [pass1, setPass1] = useState()
+    const [pass2, setPass2] = useState()
+
+    useEffect(() => {
+        validate()
+    },[pass1, pass2])
+
+    const handleNameChange = (event) => {
+        if (event.target.id === 'firstname') {
+            setFirstname(event.target.value)
+        } else {
+            setLastname(event.target.value)
+        }
+    }
+
+    const handlePassChange = (event) => {
+        if (event.target.id === 'pass1') {
+            setPass1(event.target.value)
+            validate()
+        } else {
+            setPass2(event.target.value)
+            validate()
+        }
+    }
+
+    const validate = () => {
+        const one = document.getElementById('8char')
+        const two = document.getElementById('1lett')
+        const three = document.getElementById('1num')
+        const four = document.getElementById('1spec')
+        const five = document.getElementById('passM')
+
+        if (!(pass1 && pass2)) {
+            five.style.color = 'white'
+        } else if (pass1 === pass2){
+            five.style.color = 'black'
+        }
+    }
 
     return (
         <div className='signUpCont c'>
@@ -12,19 +52,19 @@ const SignUp = (props) => {
                 <form action="">
                     <div className='wrapper mt-3'>
                         <div className='input-data' id='e'>
-                            <input required type='text'/>
+                            <input id='firstname' onChange={handleNameChange} required type='text'/>
                             <label htmlFor="">First Name</label>
                         </div>
                         <div className='input-data' id='e'>
-                            <input required type='text'/>
+                            <input id='lastname' onChange={handleNameChange} required type='text'/>
                             <label htmlFor="">Last Name</label>
                         </div>
                         <div className='input-data' id='c'>
-                            <input required type='text'/>
+                            <input onChange={handlePassChange} id='pass1' required type='text'/>
                             <label htmlFor="">Password</label>
                         </div>
                         <div className='input-data' id='a'>
-                            <input required type="text"/>
+                            <input onChange={handlePassChange} id='pass2' required type="text"/>
                             <label htmlFor="">Re Enter Password</label>
                         </div>
                         <div>
@@ -37,30 +77,30 @@ const SignUp = (props) => {
                 <div className='reqCont'>
                     <div className='reqCol'>
                         <div className='reqRow'>
-                            <span className='fas fa-check'></span>
+                            <span id='8char' className='fas fa-check'></span>
                             <div>8+ Characters</div>
                         </div>
                         <div className='reqRow'>
-                            <span className='fas fa-check'></span>
+                            <span id='1lett' className='fas fa-check'></span>
                             <div>At least one letter</div>
                         </div>
                         <div className='reqRow'>
-                            <span className='fas fa-check'></span>
+                            <span id='1num' className='fas fa-check'></span>
                             <div>At least one number</div>
                         </div>
                     </div>
                     <div className='reqCol'>
                         <div className='reqRow'>
-                                <span className='fas fa-check'></span>
+                                <span id='1spec' className='fas fa-check'></span>
                                 <div>One special character</div>
                             </div>
                             <div className='reqRow'>
-                                <span className='fas fa-check'></span>
+                                <span id='passM' className='fas fa-check'></span>
                                 <div>Passwords Match</div>
                             </div>
                         </div>
                     </div>
-            </div>
+                </div>
             </div>
         </div>
     )
