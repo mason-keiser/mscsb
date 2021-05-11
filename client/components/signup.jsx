9df3439009dcf1127cb429982583bfcdc42906d6
Mason Keiser <masonksr5@gmail.com>
2021-05-11 03:29:08 +0000

    sign up form started :

--- client/components/signup.jsx
+++ client/components/signup.jsx
@@ -7,6 +7,30 @@ const SignUp = (props) => {
     return (
         <div className='signUpCont c'>
             <Menu path={props.match.path} nightMode={props.nightMode} setNightMode={props.setNightMode}/>
+            <h2 className='sTitle'>Create Account</h2>
+            <form action="">
+                <div className='wrapper mt-3'>
+                    <div className='input-data' id='e'>
+                        <input required type='text'/>
+                        <label htmlFor="">First Name</label>
+                    </div>
+                    <div className='input-data' id='e'>
+                        <input required type='text'/>
+                        <label htmlFor="">Last Name</label>
+                    </div>
+                    <div className='input-data' id='c'>
+                        <input required type='text'/>
+                        <label htmlFor="">Password</label>
+                    </div>
+                    <div className='input-data' id='a'>
+                        <input required type="text"/>
+                        <label htmlFor="">Re Enter Password</label>
+                    </div>
+                    <div>
+                        <button className='signupBtn'>Create Account</button>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
