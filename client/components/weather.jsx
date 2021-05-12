9f3e3fe389713cc0210989a9df5f46f7108d0b78
Mason Keiser <masonksr5@gmail.com>
2021-05-12 06:01:16 +0000

    more features added to weather

--- client/components/weather.jsx
+++ client/components/weather.jsx
@@ -53,7 +53,7 @@ const Weather = (props) => {
             <Menu path={props.match.path} nightMode={props.nightMode} setNightMode={props.setNightMode}/>
             <h2 className='sTitle'>{props.mInfo.name}</h2>
             <div className='w'>
-                <img src="/images/logo4bw.png" id='wIcon' alt=""/>
+                <img src='' onError={() => {this.style.display = 'none'}} alt='' id='wIcon' />
             </div>
             {
                 (weatherInfo) ? (
@@ -68,6 +68,7 @@ const Weather = (props) => {
                 )
                 : null
             }
+            <div className='addBtn'>add</div>
             <Footer/>
         </div>
     )
